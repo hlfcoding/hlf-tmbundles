1c186403d2532e16295a4792960d972ed952a1ce
Peng Wang <peng@pengxwang.com>
2011-05-11 23:50:03 -0700

    first commit

---
+++ PHP.tmbundle/Templates/PHP Page.tmTemplate/page.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  ${TM_NEW_FILE_BASENAME}
+
+  Created by ${TM_USERNAME} on ${TM_DATE}.
+  Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>${TM_NEW_FILE_BASENAME}</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="${TM_FULLNAME}">
+	<!-- Date: ${TM_DATE} -->
+</head>
+<body>
+<?php
+	echo "Hello World";
+?>
+</body>
+</html>
